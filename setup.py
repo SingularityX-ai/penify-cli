0d4554707437357983b75ebd9a2dbeed092e3443
sumansaurabh <ss.sumansaurabh92@gmail.com>
2024-08-23 15:11:50 +0530

    feat: Add support for doc_gen_hook package

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'doc_gen_hook=doc_gen_hook.main:main',
+            'doc_gen_hook=doc_gen_hook.main:main',  # This forces the name to use underscores
         ],
     },
     author='Suman Saurabh',
     description='A post-commit hook that analyzes modified files via an API and commits changes automatically.',
-    url='https://github.com/Singualrityx-ai/doc_gen_hook',  # Update with your actual GitHub repo
+    url='https://github.com/SingualrityX-ai/doc_gen_hook',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
