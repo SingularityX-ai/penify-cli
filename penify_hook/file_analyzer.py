4d03e68f9d835b912bd4ebddf9ca67dd731f7d4a
sumansaurabh <ss.sumansaurabh92@gmail.com>
2025-03-07 03:23:25 +0530

    feat: Integrate tqdm for enhanced progress tracking in file processing

--- penify_hook/file_analyzer.py
+++ penify_hook/file_analyzer.py
@@ -1,10 +1,11 @@
 import os
 from git import Repo
+from tqdm import tqdm
 from .api_client import APIClient
 import logging
 from .ui_utils import (
-    print_info, print_success, print_warning, print_error,
-    print_processing, print_status, create_stage_progress_bar,
+    format_highlight, print_info, print_success, print_warning, print_error,
+    print_status, create_stage_progress_bar,
     update_stage, format_file_path
 )
 
@@ -17,7 +18,7 @@ class FileAnalyzerGenHook:
         self.api_client = api_client
         self.supported_file_types = set(self.api_client.get_supported_file_types())
 
-    def process_file(self, file_path, pbar):
+    def process_file(self, file_path, pbar: tqdm):
         """Process a file by reading its content and sending it to an API for
         processing.
 
@@ -35,9 +36,14 @@ class FileAnalyzerGenHook:
         """
         file_abs_path = os.path.join(os.getcwd(), file_path)
         file_extension = os.path.splitext(file_path)[1].lower()
-
+        # self.print_processing(self.file_path)
+        
+            
+        pbar.update(1)  # Complete first stage
         # Validate file
         update_stage(pbar, "Validating file")
+        
+        pbar.update(2)
         
         if not file_extension:
             logger.info(f"File {file_path} has no extension. Skipping.")
@@ -73,6 +79,12 @@ class FileAnalyzerGenHook:
             file.write(response)
         logger.info(f"Updated file {file_path} with generated documentation")
         return True
+    
+    def print_processing(self, file_path):
+        """Print a processing message for a file."""
+        formatted_path = format_file_path(file_path)
+        print(f"\n{format_highlight(f'Processing file: {formatted_path}')}")
+
 
     def run(self):
         """Run the post-commit hook.
@@ -84,16 +96,12 @@ class FileAnalyzerGenHook:
         displays a progress bar and colored output to provide visual feedback on
         the processing status.
         """
-        print_info(f"Starting file documentation for {format_file_path(self.file_path)}")
         
         # Create a progress bar with appropriate stages
-        stages = ["Validating", "Reading content", "Documenting", "Writing changes"]
-        pbar, _ = create_stage_progress_bar(stages, "Processing file")
+        stages = ["INIT","Validating", "Reading content", "Documenting", "Writing changes"]
+        pbar, _ = create_stage_progress_bar(stages, f"Starting file documentation")
         
         try:
-            print_processing(self.file_path)
-            pbar.update(1)  # Complete first stage
-            
             result = self.process_file(self.file_path, pbar)
             
             # Ensure all stages are completed
