12ba0a3c565da1baeef70da49de1f44fac9a136f
sumansaurabh <ss.sumansaurabh92@gmail.com>
2025-03-08 01:49:22 +0530

    refactor(login): streamline login handling by importing necessary constants and commands

--- penify_hook/login_command.py
+++ penify_hook/login_command.py
@@ -3,6 +3,9 @@ def setup_login_parser(parser):
     # Add all other necessary arguments for login command
     
 def handle_login(args):
+    from penify_hook.constants import API_URL, DASHBOARD_URL
+    from penify_hook.commands.auth_commands import login
+
+
     # Only import dependencies needed for login functionality here
-    from .login import process_login
-    return process_login(args)
+    return login(API_URL, DASHBOARD_URL)
