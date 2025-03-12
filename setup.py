6ef02f347fbf77ecf289c55655531606c442150f
sumansaurabh <ss.sumansaurabh92@gmail.com>
2025-03-12 06:34:11 +0530

    chore: update coverage timestamp and increment version to 0.2.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="penifycli",  # Changed from "penifycli" to a more unique name
-    version="0.2.2",
+    version="0.2.3",
     packages=['penify_hook'],
     install_requires=[
         "requests",
